918518b72271a50e5ac64acd04f3f932f23b1be7
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-03 15:53:58 -0400

    Solved it

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,16 @@
-def stock_picker
-
+def stock_picker array 
+	profit = 0 
+	for i in 0...array.length
+		for j in (i+1)...array.length
+			if((array[j] - array[i]) > profit)
+				profit = array[j] - array[i]
+				buyday = i
+				sellday = j
+			end
+		end
+	end
+	days = [buyday, sellday]
+	puts days
 end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
